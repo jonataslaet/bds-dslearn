7c30a34480382b7ff3621df6de9dc0179db63740
Jonatas Laet <jonataslaetprogramador@gmail.com>
2021-08-23 11:31:06 -0300

    Implemented entity Offer. Seeding data.sql

--- backend/src/main/java/com/devsuperior/dslearn/entities/Course.java
+++ backend/src/main/java/com/devsuperior/dslearn/entities/Course.java
@@ -1,11 +1,14 @@
 package com.devsuperior.dslearn.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -23,6 +26,9 @@ public class Course implements Serializable {
 
 	private String imgGrayUri;
 
+	@OneToMany(mappedBy = "course")
+	private List<Offer> offers = new ArrayList<>();
+	
 	public Course() {
 
 	}
