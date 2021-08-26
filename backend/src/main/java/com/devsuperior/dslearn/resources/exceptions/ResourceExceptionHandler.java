4a381b829f4b2454bbf6f2224824271729a60c59
Jonatas Laet <jonataslaetprogramador@gmail.com>
2021-08-25 21:27:08 -0300

    Implemented exceptions, validations and security

---
+++ backend/src/main/java/com/devsuperior/dslearn/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,52 @@
+package com.devsuperior.dslearn.resources.exceptions;
+
+import javax.persistence.EntityNotFoundException;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.devsuperior.dslearn.services.exceptions.DatabaseException;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+	@ExceptionHandler(EntityNotFoundException.class)
+	public ResponseEntity<StandardError> entityNotFound(EntityNotFoundException e, HttpServletRequest req){
+		StandardError error = new StandardError();
+		error.setError("Entity not found");
+		error.setMessage(e.getLocalizedMessage());
+		error.setPath(req.getServletPath());
+		error.setStatus(HttpStatus.NOT_FOUND.value());
+		return ResponseEntity.status(HttpStatus.NOT_FOUND.value()).body(error);
+	}
+	
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> databaseException(DatabaseException e, HttpServletRequest req){
+		StandardError error = new StandardError();
+		error.setError("Data restriction is being violated");
+		error.setMessage(e.getLocalizedMessage());
+		error.setPath(req.getServletPath());
+		error.setStatus(HttpStatus.BAD_REQUEST.value());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST.value()).body(error);
+	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<ValidationError> validationException(MethodArgumentNotValidException e, HttpServletRequest req){
+		ValidationError error = new ValidationError();
+		error.setError("Field validation");
+		error.setMessage(e.getLocalizedMessage());
+		error.setPath(req.getServletPath());
+		error.setStatus(HttpStatus.UNPROCESSABLE_ENTITY.value());
+		
+		for (FieldError fieldError : e.getBindingResult().getFieldErrors()) {
+			error.addError(fieldError.getField(), fieldError.getDefaultMessage());
+		}
+		
+		return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY.value()).body(error);
+	}
+}
