db3870a5f5f4a1cd8caba3377311d744de3c7501
Jonatas Laet <jonataslaetprogramador@gmail.com>
2021-08-27 14:27:24 -0300

    Including refresh token

--- backend/src/main/java/com/devsuperior/dslearn/configs/AuthorizationServerConfig.java
+++ backend/src/main/java/com/devsuperior/dslearn/configs/AuthorizationServerConfig.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
@@ -46,6 +47,9 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	@Autowired
 	private AuthenticationManager authenticationManager;
 	
+	@Autowired
+	private UserDetailsService userDetailsService;
+	
 	@Override
 	public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
 		security.tokenKeyAccess("permitAll()").checkTokenAccess("isAuthenticated()");
@@ -57,7 +61,8 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 		.withClient(clientTd)
 		.secret(bCrypt.encode(clientSecret))
 		.scopes("read", "write")
-		.authorizedGrantTypes("password")
+		.authorizedGrantTypes("password", "refresh_token")
+		.refreshTokenValiditySeconds(jwtDuration)
 		.accessTokenValiditySeconds(jwtDuration);
 	}
 
@@ -71,7 +76,7 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 		.authenticationManager(authenticationManager)
 		.tokenStore(tokenStore)
 		.accessTokenConverter(accessTokenConverter)
-		.tokenEnhancer(tokenEnhancerChain);
+		.tokenEnhancer(tokenEnhancerChain).userDetailsService(userDetailsService);
 	}
 
 	
